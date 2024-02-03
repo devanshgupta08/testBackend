bb653b45824b9014e45566d63ba80e9834681a37
Devansh Gupta <devansh00gupta@gmail.com>
2024-02-03 15:57:04 +0530

    removed cors and using proxy

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
